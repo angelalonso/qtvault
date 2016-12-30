de77e45541a9240ed7904a878b3f77688f5b0f05
angelalonso <alonsofonseca.angel@gmail.com>
2016-12-31 00:24:57 +0100

    working with dummy pass script

--- qtvault.py
+++ qtvault.py
@@ -1,22 +1,26 @@
 #!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
 """
-First draft taken from http://www.learningpython.com/2008/09/20/an-introduction-to-pyqt/#FirstApplication
+First draft taken from:
+ http://stackoverflow.com/questions/15561608/detecting-enter-on-a-qlineedit-or-qpushbutton#15567835
 TODO:
     - show password after <Enter> or better yet:  thaat it exists
-    - copy into clipboard after button click
+    - clean defs
+    - add colors to "Password Exists" and "Password copied", and a text message
 """
 
-
-from PyQt4 import QtGui
+from PyQt4 import QtGui, QtCore
 import subprocess
 import sys
 
+
 def get_passnames():
     List = []
-    cmd = "/home/aaf/Software/qtvault/test.sh pass"
+    cmd = "/home/aaf/Software/Dev/qtvault/test.sh pass"
     process = subprocess.Popen(cmd, shell=True,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE)
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE)
     # wait for the process to terminate
     out, err = process.communicate()
     errcode = process.returncode
@@ -24,44 +28,68 @@ def get_passnames():
         List.append(word)
     return List
 
-def
 
 def get_pass(passname):
     password = passname + 'blah'
     return password
 
-def textchanged(text):
-    print "contents of text box: "+text
 
+class MyWindow(QtGui.QWidget):
+    def __init__(self, parent=None):
+        super(MyWindow, self).__init__(parent)
 
-def window():
-    app = QtGui.QApplication(sys.argv)
-    widget = QtGui.QWidget()
+        self.setGeometry(412, 250, 600, 150)
 
-    label = QtGui.QLabel(widget)
-    label.move(20,20)
-    label.setText("What are you looking for?")
+        self.label = QtGui.QLabel()
+        self.label.move(20, 20)
+        self.label.setText("What are you looking for?")
 
-    model = QtGui.QStringListModel()
-    model.setStringList(get_passnames())
+        self.pushButtonOK = QtGui.QPushButton(self)
+        self.pushButtonOK.setText("Get Pass")
+        self.pushButtonOK.clicked.connect(self.on_pushButtonOK_clicked)
+        self.pushButtonOK.setAutoDefault(True)
 
-    completer = QtGui.QCompleter()
-    completer.setModel(model)
+        self.model = QtGui.QStringListModel()
+        self.model.setStringList(get_passnames())
 
-    lineedit = QtGui.QLineEdit(widget)
-    lineedit.textChanged.connect(textchanged)
-    lineedit.move(20,40)
-    lineedit.setCompleter(completer)
-    lineedit.show()
+        self.completer = QtGui.QCompleter()
+        self.completer.setModel(self.model)
 
-    btn_cncl = QtGui.QPushButton("Cancel")
+        self.lineEdit = QtGui.QLineEdit(self)
+        self.lineEdit.returnPressed.connect(self.pushButtonOK.click)
+        self.lineEdit.move(20, 40)
+        self.lineEdit.setCompleter(self.completer)
 
-    widget.setGeometry(412,250,400,150)
-    widget.setWindowTitle("QtVault")
-    widget.show()
+        self.pushButtonCopy = QtGui.QPushButton(self)
+        self.pushButtonCopy.setText("Copy Pass")
+        self.pushButtonCopy.clicked.connect(self.on_pushButtonCopy_clicked)
+        self.pushButtonCopy.setAutoDefault(True)
 
-    sys.exit(app.exec_())
+        self.layoutHorizontal = QtGui.QHBoxLayout(self)
+        self.layoutHorizontal.addWidget(self.label)
+        self.layoutHorizontal.addWidget(self.lineEdit)
+        self.layoutHorizontal.addWidget(self.pushButtonOK)
+        self.layoutHorizontal.addWidget(self.pushButtonCopy)
+
+        self.lineEdit.setFocus()
+
+    @QtCore.pyqtSlot()
+    def on_pushButtonOK_clicked(self):
+        passwd_received = get_pass(self.lineEdit.text())
+        print (passwd_received)
+
+    def on_pushButtonCopy_clicked(self):
+        passwd_received = get_pass(self.lineEdit.text())
+        clipboard = QtGui.QApplication.clipboard()
+        clipboard.setText(passwd_received)
 
 
 if __name__ == "__main__":
-    window()
+
+    app = QtGui.QApplication(sys.argv)
+    app.setApplicationName('QtVault')
+
+    main = MyWindow()
+    main.show()
+
+    sys.exit(app.exec_())
